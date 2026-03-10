44496d0eb6e265d90a0a609e4f5a97dc6f6d9a7e
ngamphong <silapakoot@gmail.com>
2026-03-10 20:59:38 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // เพิ่มบรรทัดนี้
+  basePath: '/car-marketplace', // เพิ่มบรรทัดนี้ (ชื่อ repo ของคุณ)
   images: {
+    unoptimized: true, // เพิ่มบรรทัดนี้ด้วย เพราะ GitHub Pages ไม่รองรับระบบย่อรูปของ Next.js
     domains: [
       "bferxknatakjymihgsbs.supabase.co"
     ],
